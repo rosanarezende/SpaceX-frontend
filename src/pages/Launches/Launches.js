b8e21004969f78587f64c54ea49ef67c852523c7
rosanarezende <rezende_rosana@hotmail.com>
2022-01-03 09:39:28 -0300

    add responsivity

--- src/pages/Launches/Launches.js
+++ src/pages/Launches/Launches.js
@@ -1,70 +1,90 @@
-import { useContext } from 'react'
-import { Box, Grid, Paper, Typography, CircularProgress } from '@mui/material'
+import { useContext } from "react";
+import { Box, Grid, Paper, Typography, CircularProgress } from "@mui/material";
 
-import { Context } from '../../context'
-import { useFetchData } from '../../utils/customHooks'
+import { Context } from "../../context";
+import { useFetchData } from "../../utils/customHooks";
+import { formatDate } from "../../utils/constants";
 
 export const Launches = () => {
-  const [path] = useContext(Context)
-  const [launches, loadingLaunches] = useFetchData(path)
+  const [path] = useContext(Context);
+  const [launches, loadingLaunches] = useFetchData(path);
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1, p: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        flexGrow: 1,
+        p: 2,
+      }}
+    >
       <Typography component="h2" variant="h4" mb={3}>
-        {path === 'passados' ? 'Lançamentos Anteriores' : 'Lançamentos Futuros'}
+        {path === "passados" ? "Lançamentos Anteriores" : "Lançamentos Futuros"}
       </Typography>
-      {loadingLaunches
-        ? <Box sx={{ display: 'flex', pl: 1, pb: 1, mb: 3 }}><CircularProgress /></Box>
-        : (<Grid container spacing={2} sx={{ display: 'flex', justifyContent: 'center', pl: 10, pr: 10 }}>
-          {launches?.map((launch, index) => {
-             let launchDate = new Date(launch?.date_local)
+      {loadingLaunches ? (
+        <Box sx={{ display: "flex", pl: 1, pb: 1, mb: 3 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "flex", justifyContent: "center", pl: 8, pr: 8 }}
+        >
+          {launches?.map((launch, index) => (
+            <Grid item xs={12} md={4} key={index}>
+              <Paper
+                elevation={2}
+                sx={{
+                  p: 1,
+                  margin: "auto",
+                  flexGrow: 1,
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Grid item pr={3}>
+                  <Typography
+                    component="div"
+                    variant="subtitle2"
+                    align="center"
+                  >
+                    Missão
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                    pb={1}
+                    align="center"
+                  >
+                    {launch?.name}
+                  </Typography>
+                </Grid>
 
-             let date =
-               launchDate.getDate() +
-               "/" +
-               parseInt(launchDate.getMonth() + 1) +
-               "/" +
-               launchDate.getFullYear()
-
-            return (
-              <Grid item xs={6} md={4} key={index}>
-                <Paper elevation={2} sx={{ p: 1, margin: 'auto', flexGrow: 1, display: 'flex', justifyContent: 'center'}}>
-                  <Grid item pr={3}>
-                    <Typography component="div" variant="subtitle2" align="center">
-                      Missão
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      color="text.secondary"
-                      component="div"
-                      pb={1}
-                      align="center"
-                    >
-                      {launch?.name}
-                    </Typography>
-                  </Grid>
-  
-                  <Grid item>
-                    <Typography component="div" variant="subtitle2" align="center">
-                      Data
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      color="text.secondary"
-                      component="div"
-                      align="center"
-                    >
-                      {date}
-                    </Typography>
-                  </Grid>
-                </Paper>
-              </Grid>
-            )
-
-          }
-          )}
-        </Grid>)
-      }
+                <Grid item>
+                  <Typography
+                    component="div"
+                    variant="subtitle2"
+                    align="center"
+                  >
+                    Data
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                    align="center"
+                  >
+                    {formatDate(launch?.date_local)}
+                  </Typography>
+                </Grid>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Box>
-  )
-}
+  );
+};
